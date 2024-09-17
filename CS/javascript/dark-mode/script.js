52dcd6b33ae79123f016ac45b00d00a4d515844d
HGPGerardLA5 <gerard.anazia@hgs.hiddengeniusproject.org>
2024-09-16 17:52:31 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,13 @@
 function darkMode() {
-// Your code here.
-}
+    /* document.
+    body.getElementById("mode").style.backgroundColor;*/
+
+    if (document.body.style.backgroundColor === "red"){
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+    } else {
+        document.body.style.backgroundColor = "red";
+        document.body.style.color = "blue";
+    }
+    }
+
