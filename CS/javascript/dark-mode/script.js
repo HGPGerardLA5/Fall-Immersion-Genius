4adc25a8ac8f0bf3c2fd34c53fee12f1b6968b25
HGPGerardLA5 <gerard.anazia@hgs.hiddengeniusproject.org>
2024-09-16 18:28:26 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -2,12 +2,12 @@ function darkMode() {
     /* document.
     body.getElementById("mode").style.backgroundColor;*/
 
-    if (document.body.style.backgroundColor === "red"){
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
-    } else {
-        document.body.style.backgroundColor = "red";
-        document.body.style.color = "blue";
-    }
+        if (document.body.style.backgroundColor === "red"){
+            document.body.style.backgroundColor = "white";
+            document.body.style.color = "black";
+        } else {
+            document.body.style.backgroundColor = "red";
+            document.body.style.color = "blue";
+        }
     }
 
