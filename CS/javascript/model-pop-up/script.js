e60f0425a4543eadbb2873bfac3e308f9f3a6ee6
HGPGerardLA5 <gerard.anazia@hgs.hiddengeniusproject.org>
2024-09-24 18:51:41 -0700

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,23 @@
-function modelPopUp() {
-    // Your code here.
-}   
+const modal = document.getElementById("myModal");
+
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const alertBtn = document.getElementById("alertBtn");
+
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+alertBtn.onclick = function() {
+    alert("Oh nahh nahhh nahhhhhh");
+}
