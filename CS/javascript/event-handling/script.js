56f3ac1750b82b95c318c96fa8682eae440f5e4f
HGPGerardLA5 <gerard.anazia@hgs.hiddengeniusproject.org>
2024-10-01 18:47:15 -0700

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,5 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function() {
+    image = document.createElement('img');
+    image.src = 'https://voyagela.com/wp-content/uploads/2023/01/c-PersonalCraigTinsley__CT13_1671313452989.jpg';
+    document.body.appendChild(image);
+});
